d90a9848c6e0b9a71aba8cc86ac05ee5fda46efa
devsanbid <devsanbid@gmail.com>
2025-03-03 20:49:01 +0545

    new config

--- config/ags/config.js
+++ config/ags/config.js
@@ -1,25 +1,31 @@
-const main = "/tmp/ags/main.js"
-const entry = `${App.configDir}/main.ts`
+"use strict";
+import GLib from 'gi://GLib';
+import App from 'resource:///com/github/Aylur/ags/app.js'
+import userOptions from './modules/.configuration/user_options.js';
+import Overview from './modules/overview/main.js';
 
-const v = {
-    ags: pkg.version.split(".").map(Number),
-    expect: [1, 8, 0],
-}
+const COMPILED_STYLE_DIR = `${GLib.get_user_config_dir()}/ags/user/`
 
-try {
-    await Utils.execAsync([
-        "bun", "build", entry,
-        "--outfile", main,
-        "--external", "resource://*",
-        "--external", "gi://*",
-        "--external", "file://*",
-    ])
-    if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
-        print(`my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`)
-        App.quit()
-    }
-    await import(`file://${main}`)
-} catch (error) {
-    console.error(error)
-    App.quit()
+async function applyStyle() {
+
+    App.resetCss();
+    App.applyCss(`${COMPILED_STYLE_DIR}/style.css`);
+    console.log('[LOG] Styles loaded')
 }
+applyStyle().catch(print);
+
+const Windows = () => [
+    Overview()
+];
+const CLOSE_ANIM_TIME = 210;
+App.config({
+    css: `${COMPILED_STYLE_DIR}/style.css`,
+    stackTraceOnError: true,
+    closeWindowDelay: {
+        'sideright': CLOSE_ANIM_TIME,
+        'sideleft': CLOSE_ANIM_TIME,
+        'osk': CLOSE_ANIM_TIME,
+    },
+    windows: Windows().flat(1),
+});
+
